7d3e334d39184517ec494e65d5ae7fe97c1dd2d6
pjprateekjoshi <pjprateekjoshi@gmail.com>
2017-09-30 09:12:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 
 ////
 app.get('/ui/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 ////
 
